9d40b3b2418746dbb545074bee626ac629ec364a
Kamrul Hassan <kamrul@openrefactory.com>
2023-01-25 13:04:30 +0600

    Fix Redundant 'final' Modifier for Methods inside 'final' Class
    In file: /workspace/source/src/main/java/baritone/utils/InputOverrideHandler.java, the final class InputOverrideHandler contains methods with redundant {final} modifier. According to <hyperlink1>, method declarations inside {final} class are {final} by default. iCR suggested to remove the redundant {final} modifier.

--- src/main/java/baritone/utils/InputOverrideHandler.java
+++ src/main/java/baritone/utils/InputOverrideHandler.java
@@ -60,7 +60,7 @@ public final class InputOverrideHandler extends Behavior implements IInputOverri
      * @return Whether or not it is being forced down
      */
     @Override
-    public final boolean isInputForcedDown(Input input) {
+    public boolean isInputForcedDown(Input input) {
         return input == null ? false : this.inputForceStateMap.getOrDefault(input, false);
     }
 
@@ -71,7 +71,7 @@ public final class InputOverrideHandler extends Behavior implements IInputOverri
      * @param forced Whether or not the state is being forced
      */
     @Override
-    public final void setInputForceState(Input input, boolean forced) {
+    public void setInputForceState(Input input, boolean forced) {
         this.inputForceStateMap.put(input, forced);
     }
 
@@ -79,12 +79,12 @@ public final class InputOverrideHandler extends Behavior implements IInputOverri
      * Clears the override state for all keys
      */
     @Override
-    public final void clearAllKeys() {
+    public void clearAllKeys() {
         this.inputForceStateMap.clear();
     }
 
     @Override
-    public final void onTick(TickEvent event) {
+    public void onTick(TickEvent event) {
         if (event.getType() == TickEvent.Type.OUT) {
             return;
         }
