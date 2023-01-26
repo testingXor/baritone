b0b07f44d61523252fe70c75e1cb1b5e0421454e
Kamrul Hassan <kamrul@openrefactory.com>
2023-01-27 03:46:43 +0600

    Fix Null Dereference Check
    In file: /workspace/source/src/api/java/baritone/api/utils/SettingsUtil.java, class: SettingsUtil, method: parseAndApply there is a potential Null pointer dereference. This may throw an unexpected null pointer exception which, if unhandled, may crash the program. iCR detected the null pointer issue and demonstrated the full path from the object declaration to the null dereference in the object. A developer should introduce null checks in the appropriate path or initialize the object explicitly.

--- src/api/java/baritone/api/utils/SettingsUtil.java
+++ src/api/java/baritone/api/utils/SettingsUtil.java
@@ -196,7 +196,18 @@ public class SettingsUtil {
         Class intendedType = setting.getValueClass();
         ISettingParser ioMethod = Parser.getParser(setting.getType());
         Object parsed = ioMethod.parse(new ParserContext(setting), settingValue);
-        if (!intendedType.isInstance(parsed)) {
+        /* ********OpenRefactory Warning********
+		 Possible null pointer dereference!
+		 Path: 
+			File: SettingsUtil.java, Line: 196
+				Class intendedType=setting.getValueClass();
+		
+			File: SettingsUtil.java, Line: 199
+				intendedType.isInstance(parsed)
+				intendedType is referenced in method invocation.
+				The expression is enclosed inside an If statement.
+		*/
+		if (!intendedType.isInstance(parsed)) {
             throw new IllegalStateException(ioMethod + " parser returned incorrect type, expected " + intendedType + " got " + parsed + " which is " + parsed.getClass());
         }
         setting.value = parsed;
