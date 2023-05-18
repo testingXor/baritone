395784981b98f2264ee6d507f1c49d0da6708b23
testingXor <testing@openrefactory.com>
2023-05-18 03:26:09 +0000

    Fix Null Dereference Check
    In file: BuilderProcess.java, class: BuilderProcess, there is a method placementGoal that there is a potential Null pointer dereference. This may throw an unexpected null pointer exception which, if unhandled, may crash the program. iCR detected the null pointer issue and demonstrated the full path from the object declaration to the null dereference in the object. A developer should introduce null checks in the appropriate path or initialize the object explicitly.

--- src/main/java/baritone/process/BuilderProcess.java
+++ src/main/java/baritone/process/BuilderProcess.java
@@ -767,7 +767,16 @@ public final class BuilderProcess extends BaritoneProcessHelper implements IBuil
         boolean allowSameLevel = ctx.world().getBlockState(pos.up()).getBlock() != Blocks.AIR;
         IBlockState current = ctx.world().getBlockState(pos);
         for (EnumFacing facing : Movement.HORIZONTALS_BUT_ALSO_DOWN_____SO_EVERY_DIRECTION_EXCEPT_UP) {
-            //noinspection ConstantConditions
+            
+			/* ********OpenRefactory Warning********
+			 Possible null pointer dereference!
+			 Path: 
+				File: BuilderProcess.java, Line: 771
+					bcc.getSchematic(pos.getX(),pos.getY(),pos.getZ(),current).getBlock()
+					Method getSchematic may return null and is referenced in method invocation.
+					The expression is enclosed inside an If statement.
+			*/
+			//noinspection ConstantConditions
             if (MovementHelper.canPlaceAgainst(ctx, pos.offset(facing)) && ctx.world().mayPlace(bcc.getSchematic(pos.getX(), pos.getY(), pos.getZ(), current).getBlock(), pos, false, facing, null)) {
                 return new GoalAdjacent(pos, pos.offset(facing), allowSameLevel);
             }
